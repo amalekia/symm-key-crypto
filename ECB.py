fa25f8c3c7f3c5d3f10dc8e52001d9285e1b7251
adrickmalekian <adrickmalekian218@gmail.com>
2024-10-03 10:15:37 -0700

    ECB done

--- ECB.py
+++ ECB.py
@@ -10,15 +10,29 @@ def encrypt(key, data):
         data = data[:i] + cipher.encrypt(block) + data[i+16:]
     return cipher.encrypt(data)
 
+def decrypt(key, data):
+    cipher = AES.new(key, AES.MODE_ECB)
+    for i in range(0, len(data), 16):
+        block = data[i:i+16]
+        data = data[:i] + cipher.decrypt(block) + data[i+16:]
+    return cipher.decrypt(data)
+
 def generate_key():
     return bytes([random.randint(0, 255) for _ in range(16)])
 
 if __name__ == '__main__':
-    with open('/path/to/your/inputfile', 'rb') as f:
+    with open('./plaintext.txt', 'rb') as f:
         plaintext = f.read()
 
-    key = generate_key()                                  # This key myst be 16 bytes long
+    key = generate_key()                                        # This key myst be 16 bytes long
     ciphertext = encrypt(key, plaintext)
 
-    with open('/path/to/your/outputfile', 'wb') as f:
+    with open('./ciphertext.txt', 'wb') as f:
         f.write(ciphertext)
+
+    with open('./ciphertext.txt', 'rb') as f:
+        ciphertext = f.read()
+    
+    with open('./decrypted.txt', 'wb') as f:
+        decrypted = decrypt(key, ciphertext)
+        f.write(decrypted)
