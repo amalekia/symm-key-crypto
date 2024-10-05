d61f2dd14ff92c94cd54b8ec2abe868d531e54ea
adrickmalekian <adrickmalekian218@gmail.com>
2024-10-05 13:28:37 -0700

    ECB now preserves header, but decrypted text has null values at the end

--- CBC.py
+++ CBC.py
@@ -7,6 +7,15 @@ def generate_IV():
 def generate_key():
     return bytes([random.randint(0, 255) for _ in range(16)])
 
+def encrypt(key, iv, data):
+    cipher = AES.new(key, AES.MODE_CBC, iv=iv)
+    for i in range(0, len(data), 16):
+        block = data[i:i+16]
+        if len(block) < 16:
+            block += bytes([16 - len(block)]) * (16 - len(block))
+        data = data[:i] + cipher.encrypt(block) + data[i+16:]
+    return cipher.encrypt(data)
+
 
 if __name__ == '__main__':
     with open('./plaintext.txt', 'rb') as f:
--- ECB.py
+++ ECB.py
@@ -2,20 +2,24 @@ from Crypto.Cipher import AES
 import random
 
 def encrypt(key, data):
+    header = data[:54]
+    data = data[54:]
     cipher = AES.new(key, AES.MODE_ECB)
     for i in range(0, len(data), 16):                               # Goes through 16 bytes of data at a time
         block = data[i:i+16]
         if len(block) < 16:
             block += bytes([16 - len(block)]) * (16 - len(block))   # Pads the block with the number of bytes missing PKCS7
         data = data[:i] + cipher.encrypt(block) + data[i+16:]       # Appends the encrypted block to the data
-    return cipher.encrypt(data)
+    return header + cipher.encrypt(data)
 
 def decrypt(key, data):
+    header = data[:54]
+    data = data[54:]
     cipher = AES.new(key, AES.MODE_ECB)
     for i in range(0, len(data), 16):
         block = data[i:i+16]
         data = data[:i] + cipher.decrypt(block) + data[i+16:]
-    return cipher.decrypt(data)
+    return header + cipher.decrypt(data)
 
 def generate_key():
     return bytes([random.randint(0, 255) for _ in range(16)])
