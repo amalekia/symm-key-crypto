6c9050b96726923353234d45b1570fb649739b72
adrickmalekian <adrickmalekian218@gmail.com>
2024-10-06 19:17:21 -0700

    CBC is done

--- CBC.py
+++ CBC.py
@@ -1,32 +1,79 @@
 from Crypto.Cipher import AES
-import random
-
-def generate_IV():
-    return bytes([random.randint(0, 255) for _ in range(16)])
+import os
 
 def generate_key():
-    return bytes([random.randint(0, 255) for _ in range(16)])
+    return os.urandom(16)
+
+def generate_iv():
+    return os.urandom(16)
 
 def submit(string):
     string.insert(0, "userid=456; userdata=")
     string.append(";session-id=31337")
 
-def encrypt(key, iv, data):
+def encrypt_data(key, iv, data, header_size=54):
+    # we might want to change header_size to 138 depending
+    bmp_header = data[:header_size] 
+    # contains the rest of the plaintext data
+    plaintext_data = data[header_size:]
+    
+    # Padding to ensure length of data is
+    # multiple of 16 bytesm, otherwise adds it to the end
+    padding_length = 16 - (len(plaintext_data) % 16)
+    plaintext_data += bytes([padding_length]) * padding_length
+    
+    # the actual cipher used for symmetric encryption
     cipher = AES.new(key, AES.MODE_CBC, iv=iv)
-    for i in range(0, len(data), 16):
-        block = data[i:i+16]
-        if len(block) < 16:
-            block += bytes([16 - len(block)]) * (16 - len(block))
-        data = data[:i] + cipher.encrypt(block) + data[i+16:]
-    return cipher.encrypt(data)
 
+    # will append the encrypted ciphers to this byte string
+    # and then append this whole string to the output file
+    ciphertext = b""
+    
+    for i in range(0, len(data), 16):
+        block = plaintext_data[i:i+16]
+        encrypted_block = cipher.encrypt(block)
+        ciphertext += encrypted_block
+
+    # we need to append the iv so that it can be used for decryption
+    # steps later on
+    return bmp_header + iv + ciphertext
+
+def decrypt_data(key, data, header_size=54):
+    bmp_header = data[:header_size]
+    # get the iv that we prepended in the encryption steps
+    iv = data[header_size:header_size + 16] 
+    encrypted_data = data[header_size + 16:]
+    
+    cipher = AES.new(key, AES.MODE_CBC, iv=iv)
+    # creating a plaintext string
+    plaintext_padded = b""
+    
+    # looping through 16 byte intervals since we know
+    # this data is already padded properly
+    for i in range(0, len(data), 16):
+        block = encrypted_data[i:i+16]
+        decrypted_block = cipher.decrypt(block)
+        plaintext_padded += decrypted_block
+
+    #Unpadding
+    padding_length = plaintext_padded[-1]
+    plaintext = plaintext_padded[:-padding_length]
+    
+    return bmp_header + plaintext
 
 if __name__ == '__main__':
     with open('./plaintext.txt', 'rb') as f:
         plaintext = f.read()
 
-    iv = generate_IV()
-    key = generate_key()
+    key = generate_key()                                            # This key myst be 16 bytes long
+    ciphertext = encrypt_data(key, generate_iv(), plaintext)
 
-    while iv == key:                                        # IV and key must be different
-        iv = generate_IV()
+    with open('./ciphertext.txt', 'wb') as f:
+        f.write(ciphertext)
+
+    with open('./ciphertext.txt', 'rb') as f:
+        ciphertext = f.read()
+    
+    with open('./decrypted.txt', 'wb') as f:
+        decrypted = decrypt_data(key, ciphertext)
+        f.write(decrypted)
