2c05e3f21838fa80bdca35ccc92dbda4ec46995d
Vibhu Garg <vibhugarg@Vibhus-MacBook-Pro.local>
2024-10-08 11:43:12 -0700

    bit flipping attack

--- CBC.py
+++ CBC.py
@@ -4,6 +4,7 @@ import urllib.parse
 
 key = None
 iv = None
+global_bytes_string = None
 
 def generate_key():
     return os.urandom(16)
@@ -83,6 +84,47 @@ def decrypt_data_P2(key, data):
     
     return plaintext
 
+
+def get_ciphertext_length(ciphertext):
+    
+    return len(ciphertext)
+
+def flipping_bit_attack(ciphertext):
+    res = None
+    ciphertext_length = get_ciphertext_length(ciphertext)
+
+    # find admin= in global_bytes_string after decoding it first
+    adminIndex = global_bytes_string.find(b'admin%3D')
+
+    #convert bytestring to regular string
+    regular_string = global_bytes_string.decode()
+    # ciphertext length should be matching 
+    if ciphertext_length > adminIndex:
+        print("dsd" , regular_string[adminIndex])
+        i = adminIndex + 8
+        if regular_string[i] == 't':
+            res = global_bytes_string
+            print("already true ", res)
+        elif regular_string[i] == 'f':
+            # We need to flip the bit to change 'f' to 't'
+            # XOR the byte at index i with the difference
+            f_ascii = ord('f')  
+            t_ascii = ord('t')  
+            difference = t_ascii - f_ascii  
+
+            # Convert the ciphertext to a mutable bytearray
+            modified_ciphertext = bytearray(ciphertext)
+
+            # XOR the byte at position `adminIndex + 8` to change 'f' to 't'
+            modified_ciphertext[adminIndex + 8] ^= difference
+
+            # Return the modified ciphertext as a bytes string
+            res = (bytes(modified_ciphertext))
+            print("already false now is true ", res)
+        
+    return res
+
+
 def submit(string):
     # URL encode the string using quote function in urllib.parse
     string = "userid=456;userdata=" + urllib.parse.quote(string) + ";session-id=31337" 
@@ -91,20 +133,24 @@ def submit(string):
     padding_length = block_size - (len(string) % block_size)
     padded_string = string + chr(padding_length) * padding_length
     bytes_string = bytes(padded_string, 'utf-8')
+    global global_bytes_string
+    global_bytes_string = bytes_string
+    print("length of string in submit aftert padding is " , len(bytes_string))
     return encrypt_data_P2(key, iv, bytes_string)
 
-def verify(string):
+def verify(param):
+    print("verify string is " , param)
     # Unpadding the string using PKCS#7
-    padding_length = string[-1]
-    string[:-padding_length]
+    padding_length = param[-1]
+    param[:-padding_length]
 
     # Decrypt the string
-    decrypted = decrypt_data_P2(key, string)
+    decrypted = decrypt_data_P2(key, param)
     print(f'Decrypted: {decrypted}')
-    decrypted_str = str(decrypted, 'utf-8')
+    decrypted_str = decrypted.decode()
 
     # Check if the string contains "admin=true"
-    return ";admin=true;" in decrypted_str
+    return "admin%3Dtrue" in decrypted_str
 
 def decrypt_data(key, data, header_size=54):
     bmp_header = data[:header_size]
@@ -153,7 +199,10 @@ if __name__ == '__main__':
     # part 2
     with open('./plaintextP2.txt', 'rb') as f:
         plaintextP2 = f.read()
-        print(f'Original plaintext: {plaintextP2}\n')
         ciphertext = submit(plaintextP2)
-        print(f'Generated ciphertext: {ciphertext}\n')
+        print(type(ciphertext))
         print(f'Verifying if admin=true: {verify(ciphertext)}\n')
+        # hacked_cipher_text is some bytes string
+        hacked_cipher_text = flipping_bit_attack(ciphertext)
+        print(type(hacked_cipher_text))
+        print(f'Verifying if admin=true after flipping bit: {verify(hacked_cipher_text)}\n')
